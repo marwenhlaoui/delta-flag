3a43343eeb2373a8626b40a2c79c41eedf9a69c2
marwenhlaoui <marwenhlaoui@gmail.com>
2017-04-18 12:14:21 +0100

    notif navbar

--- resources/views/layouts/v1/_include/_navbar.blade.php
+++ resources/views/layouts/v1/_include/_navbar.blade.php
@@ -37,16 +37,53 @@
         <li><a href="#">Link</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+          <span class="nav-icon ion ion-android-notifications">
+            <i class="badge">5</i>
+          </span>
+
+          </a>
+          <ul class="dropdown-menu nav-list-menu" role="menu">
+            <li>
+              <a href="#">
+                  <span class="ion ion-lightbulb text-warning"></span> 
+                  <b>Lorem ipsum dolor sit amet</b>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <i>3 min ago</i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                  <span class="ion ion-leaf text-success"></span> 
+                  <b>Lorem ipsum dolor sit amet</b>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <i>3 min ago</i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                  <span class="ion ion-bug text-danger"></span> 
+                  <b>Lorem ipsum dolor sit amet</b>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <i>3 min ago</i>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
           <img src="{{ asset('layouts/v1/img/profile/user2.png') }}" class="nav-img">
           User fullname <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Activity Log</a></li> 
+            <li><a href="#"><i class="ion ion-person"></i> &nbsp; Profile</a></li>
+            <li><a href="#"><i class="ion ion-radio-waves"></i> &nbsp; Activity Log</a></li> 
             <li class="divider"></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Security</a></li>
+            <li><a href="#"><i class="ion ion-gear-b"></i> &nbsp; Settings</a></li>
+            <li><a href="#"><i class="ion ion-nuclear"></i> &nbsp; Security</a></li>
             <li class="divider"></li>
-            <li><a href="#">Logout</a></li>
+            <li><a href="#"><i class="ion ion-power"></i> &nbsp; Logout</a></li>
           </ul>
         </li>
       </ul>
